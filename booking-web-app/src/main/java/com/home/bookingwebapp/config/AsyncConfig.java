d6870de98edaf64ed92b04348c706a84f2e0774e
AvProphet <olegarx2000@gmail.com>
2021-05-18 20:03:50 +0200

    Added spring security, will change it for smth more secure. Idk what the hell is worn with rabbitMq, but it doesnt work for me.

---
+++ booking-web-app/src/main/java/com/home/bookingwebapp/config/AsyncConfig.java
@@ -0,0 +1,50 @@
+package com.home.bookingwebapp.config;
+
+
+import com.home.bookingwebapp.async.RoomCleanerListener;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AsyncConfig {
+    private static final String QUEUE_NAME = "room-cleaner";
+    private static final String EXCHANGE_NAME = "operations";
+
+    @Bean
+    public Queue queue(){
+        return new Queue(QUEUE_NAME, false);
+    }
+
+    @Bean
+    public TopicExchange exchange(){
+        return new TopicExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange){
+        return BindingBuilder.bind(queue).to(exchange).with("rooms.clean");
+    }
+
+    @Bean
+    public MessageListenerAdapter listenerAdapter(RoomCleanerListener listener){
+        return new MessageListenerAdapter(listener, "receiveMessage");
+    }
+
+    @Bean
+    public SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter){
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        connectionFactory.createConnection().createChannel(true);
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(QUEUE_NAME);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+}
